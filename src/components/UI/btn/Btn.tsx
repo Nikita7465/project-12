8ffbc6b876a16e8522803faf5b60c1b3c79041c2
Nikita <06nekit13@gmail.com>
2023-06-23 11:01:14 +0600

    add btn

---
+++ src/components/UI/btn/Btn.tsx
@@ -0,0 +1,11 @@
+import styles from "./btn.module.css";
+
+const Btn = () => {
+  return (
+    <div>
+      <button className={styles.btn}>More</button>
+    </div>
+  );
+};
+
+export default Btn;
---
+++ src/components/card-more/CardMore.tsx
@@ -0,0 +1,7 @@
+import styles from "./cardMore.module.css";
+
+const CardMore = () => {
+  return <div className={styles.cardMore}></div>;
+};
+
+export default CardMore;
